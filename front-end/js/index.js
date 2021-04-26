cb0796d19732c9932b145457e3277e8bd14e7196
Gérald <gerald.navarette@gmail.com>
2021-04-26 09:13:05 +0200

    improve style

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -22,10 +22,14 @@ function displayProducts(product) {
     const templateElt = document.getElementById("templateProduct")
     const cloneElt = document.importNode(templateElt.content, true)
 
+    cloneElt.getElementById("product__image").innerHTML = "<img src= " + product.imageUrl + ">" 
     cloneElt.getElementById("product__name").textContent = product.name
     cloneElt.getElementById("product__price").textContent = (product.price/1000).toFixed(2) + " €"
     cloneElt.getElementById("product__description").textContent = product.description
-    cloneElt.getElementById("product__link").innerHTML = "<a href=" + 'product.html' + '?' + 'id=' + product._id + ">" + 'Produit' + "</a>"  
+    // cloneElt.getElementById("product__link").innerHTML = "<a href=" + 'product.html' + '?' + 'id=' + product._id + ">" + 'Produit' + "</a>"
+    cloneElt.getElementById('product__card').onclick = function() {
+        window.location='product.html' + '?' + 'id=' + product._id
+    }  
     document.getElementById('main').appendChild(cloneElt)       
 }
 
